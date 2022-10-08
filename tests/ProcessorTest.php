ddcfc877b5c1702dcef37df0db2c1625ca9ac609
Rachmat Ardico Perdana <rahmatardiko99@gmail.com>
2022-10-08 16:35:59 +0700

    Processor

---
+++ tests/ProcessorTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Matt\PhpLogging;
+
+use Monolog\Handler\StreamHandler;
+use Monolog\Logger;
+use Monolog\Processor\GitProcessor;
+use Monolog\Processor\HostnameProcessor;
+use Monolog\Processor\MemoryUsageProcessor;
+use PHPUnit\Framework\TestCase;
+
+class ProcessorTest extends TestCase
+{
+    public function testProcessorRecord()
+    {
+        $logger = new Logger(ProcessorTest::class);
+        $logger->pushHandler(new StreamHandler("php://stderr"));
+        $logger->pushProcessor(new GitProcessor());
+        $logger->pushProcessor(new MemoryUsageProcessor());
+        $logger->pushProcessor(new HostnameProcessor());
+        $logger->pushProcessor(function ($record){
+            $record["extra"]["pzn"] = [
+                "app"       => "Belajar PHP Logging",
+                "author"    => "Programmer Zaman Now"
+            ];
+            return $record;
+        });
+
+        $logger->info("Hello World");
+
+        self::assertNotNull($logger);
+    }
+}
